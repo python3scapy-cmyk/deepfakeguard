ff76ea2a0276ee39b9f0d8405cfc48e0b54c9fe8
python3scapy-cmyk <python3scapy@gmail.com>
2026-07-11 13:49:17 +0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -960,4 +960,4 @@ def on_ice_candidate(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
